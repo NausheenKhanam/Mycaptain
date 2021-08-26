33aa4cee30130de7ba583ce8b8271b92a879eb40
NausheenKhanam <85051771+NausheenKhanam@users.noreply.github.com>
2021-08-27 01:33:55 +0530

    Add files via upload

---
+++ WAP to perfom different set of operations.py
@@ -0,0 +1,14 @@
+X = {0, 2, 4, 6, 8};
+Y = {1, 2, 3, 4, 5};
+  
+# union
+print("Union of X,Y :", X| Y)
+  
+# intersection
+print("Intersection of X,Y:", X & Y)
+  
+# difference
+print("Difference of X,Y :", X - Y)
+  
+# symmetric difference
+print("Symmetric difference of X,Y :", X ^ Y)
